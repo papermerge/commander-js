8dd1a6745d82ca5d4e76745fe1e57fb92c0f62c8
Eugen Ciur <eugen@papermerge.com>
2021-04-08 09:43:58 +0200

    add nunjucks dependency

--- document-commander/renderman.js
+++ document-commander/renderman.js
@@ -1,7 +1,9 @@
+import { render as original_render } from "nunjucks";
 
 
-function render(template_name, object) {
-
+function render(template_name, context) {
+    return original_render(template_name, context);
 }
+
 
 export { render };
