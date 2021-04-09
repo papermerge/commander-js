ef4f1034908a8cc2bd50d8017dac8e959b8ed9f5
Eugen Ciur <eugen@papermerge.com>
2021-04-09 16:01:23 +0200

    Document commander, features descripion

--- site/index.py
+++ site/index.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+app.jinja_env.auto_reload = True
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/basic-rendering')
+def basic_rendering():
+    return render_template("features/basic-rendering.html")
 
 
 @app.route('/about')
