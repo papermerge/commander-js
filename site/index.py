1da7e9dee17a32996dc5eb8afa627e5e8ef03034
Eugen Ciur <eugen@papermerge.com>
2021-04-18 06:49:15 +0200

    prepare server side to handle basic ajax/plain html request/respose

--- site/index.py
+++ site/index.py
@@ -1,8 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 app.jinja_env.auto_reload = True
 
+FOLDERS = {
+    3: {
+        'current_nodes': [
+            {'title': 'mydoc1.pdf', 'id': 5},
+            {'title': 'mydoc2.pdf', 'id': 6}
+        ],
+        'ancestor_nodes': [ {'title': 'My Documents', 'id': 3} ] 
+    }
+}
+
+DOCUMENTS = {
+    1: {
+        'document': {
+            'id': 1,
+            'title': 'payment_1.pdf' 
+        },
+        'ancestor_nodes': []
+    },
+    2: {
+        'document': {
+            'id': 2,
+            'title': 'payment_2.pdf' 
+        },
+        'ancestor_nodes': []
+    },
+    5: {
+        'document': {
+            'id': 5,
+            'title': 'invoice.pdf' 
+        },
+        'ancestor_nodes': []
+    },
+}
 
 @app.route('/')
 def index():
@@ -27,3 +60,43 @@ def mini_browser():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/folder/<int:node_id>')
+def mini_browser_folder(node_id):
+    current_nodes, ancestor_nodes = FOLDERS.get(node_id, (None, None))
+    if not current_nodes:
+        return render_template("404.html"), 404
+
+    request_xhr_key = request.headers.get('X-Requested-With')
+    if request_xhr_key and request_xhr_key == 'XMLHttpRequest':
+       return {
+           'current_nodes': current_nodes,
+           'ancestor_nodes': ancestor_nodes
+       }
+
+    return render_template(
+        "features/mini-browser.html",
+        current_nodes=current_nodes,
+        ancestor_nodes=ancestor_nodes
+    )
+
+
+@app.route('/document/<int:node_id>')
+def mini_browser_document(node_id):
+    document, ancestor_nodes = DOCUMENTS.get(node_id, (None, None))
+    if not document:
+        return render_template("404.html"), 404
+
+    request_xhr_key = request.headers.get('X-Requested-With')
+    if request_xhr_key and request_xhr_key == 'XMLHttpRequest':
+       return {
+           'document': current_nodes,
+           'ancestor_nodes': ancestor_nodes
+       }
+
+    return render_template(
+        "features/mini-browser.html",
+        document=current_nodes,
+        ancestor_nodes=ancestor_nodes
+    )
