9f7465751324beb5b74504e139eca5242ebe97b8
Eugen Ciur <eugen@papermerge.com>
2021-04-20 09:32:54 +0200

    some comments never hurt

--- document-commander/lib/collection.js
+++ document-commander/lib/collection.js
@@ -27,7 +27,7 @@ class Collection extends Array {
         this.trigger("change");
     }
 
-    get(_attrs) {
+    get(attrs) {
         /**
          * Returns exactly one item of the collection that matches given
          * set of attributes.
@@ -39,14 +39,19 @@ class Collection extends Array {
          * 
          *  // returns item that matches by id
          *  collection.get({id: "101"})
+         * 
+         * Note that passed attributes with value 'undefined' will be ignored
+         * i.e. col.get({id: 1}) is same as col.get({id: 1, title: undefined})
+         * because title attribute will be ignored (because its
+         * value is undefined).
          */
         for(let i=0; i < this.length;  i++) {
             let found = true;
-            for(let key in _attrs) {
-                if (_attrs[key] == undefined) {
+            for(let key in attrs) {
+                if (attrs[key] == undefined) {
                     continue;
                 }
-                if (_attrs[key] != this[i][key]) {
+                if (attrs[key] != this[i][key]) {
                     found = false;
                 }
             }
