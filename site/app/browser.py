38092c71df8d6f8bbe6191ac1d67167c4fc56688
Eugen Ciur <eugen@papermerge.com>
2021-04-25 08:35:06 +0200

    reuse code via flask blueprints

---
+++ site/app/browser.py
@@ -0,0 +1,149 @@
+import os
+
+from flask import (
+    Blueprint,
+    render_template,
+    request
+)
+
+
+def _get_template_name(req):
+    name_with_slashes = req.url_rule.rule
+    template_name = name_with_slashes.replace('/', '')
+    
+    return f"{template_name}.html"
+
+def _static_folder_abs_path():
+    """
+    Returns absolute path to the static folder.
+    Static folder is same as the one used by the app.
+    """
+    abs_path = os.path.join(os.path.dirname(__file__), '..', 'static')
+    return abs_path
+
+
+blueprint = Blueprint(
+    'reusable_browsing_app',
+    __name__,
+    template_folder='templates',
+    static_folder=_static_folder_abs_path()
+
+)
+
+
+
+# Mocks server responses for GET /folder/<int:folder_id>
+FOLDERS = {
+    -1: {
+        'current_nodes': [
+            {'title': 'invoice.pdf', 'id': 5, 'model': 'document'},
+            {'title': 'payment_1.pdf', 'id': 1, 'model': 'document'},
+            {'title': 'payment_2.pdf', 'id': 2, 'model': 'document'},
+            {'title': 'My Documents', 'id': 3, 'model': 'folder'},
+        ],
+        # All ancestors are folders; thus there is no need to specify
+        # model attribute.
+        'ancestor_nodes': [  ]
+    },
+    3: {
+        # notice 'model' attribute which specifies type of node either document
+        # or folder. There can be only two types of nodes: 'document' and
+        # 'folder'
+        'current_nodes': [
+            {'title': 'mydoc1.pdf', 'id': 5, 'model': 'document'},
+            {'title': 'mydoc2.pdf', 'id': 6, 'model': 'document'},
+            {'title': 'Some Folder', 'id': 7, 'model': 'folder'},
+        ],
+        # All ancestors are folders; thus there is no need to specify
+        # model attribute.
+        'ancestor_nodes': [ {'title': 'My Documents', 'id': 3} ]
+    },
+    7: {
+        'current_nodes': [
+            {'title': 'inv1.pdf', 'id': 8, 'model': 'document'},
+            {'title': 'inv2.pdf', 'id': 9, 'model': 'document'},
+            {'title': 'inv3.pdf', 'id': 10, 'model': 'document'},
+            {'title': 'inv4.pdf', 'id': 11, 'model': 'document'},
+        ],
+        # All ancestors are folders; thus there is no need to specify
+        # model attribute.
+        'ancestor_nodes': [
+            {'title': 'My Documents', 'id': 3},
+            {'title': 'Some Folder', 'id': 7},
+        ]
+    }
+}
+
+# Mocks server resonses for GET /document/<int:document_id>
+DOCUMENTS = {
+    1: {
+        'document': {
+            'id': 1,
+            'title': 'payment_1.pdf' 
+        },
+        'ancestor_nodes': []
+    },
+    2: {
+        'document': {
+            'id': 2,
+            'title': 'payment_2.pdf' 
+        },
+        'ancestor_nodes': []
+    },
+    5: {
+        'document': {
+            'id': 5,
+            'title': 'invoice.pdf' 
+        },
+        'ancestor_nodes': []
+    },
+}
+
+
+@blueprint.route('/')
+def browser():
+    template_name = f"features/{_get_template_name(request)}"
+    return render_template(template_name)
+
+
+@blueprint.route('/folder/')
+def browser_root_folder():
+    content_type = request.headers.get('Content-Type')
+    if content_type and content_type == 'application/json':
+        return FOLDERS.get(-1)
+
+
+@blueprint.route('/folder/<int:node_id>')
+def browser_folder(node_id):
+    template_name = f"features/{_get_template_name(request)}"
+
+    folder_dict = FOLDERS.get(node_id, None)
+    if not folder_dict:
+        return render_template("404.html"), 404
+
+    content_type = request.headers.get('Content-Type')
+    if content_type and content_type == 'application/json':
+       return folder_dict
+
+    return render_template(
+        template_name, **folder_dict
+    )
+
+
+@blueprint.route('/document/<int:node_id>')
+def browser_document(node_id):
+    template_name = f"features/{_get_template_name(request)}"
+    document_dict = DOCUMENTS.get(node_id, None)
+    
+    if not document_dict:
+        return render_template("404.html"), 404
+
+    content_type = request.headers.get('Content-Type')
+    if content_type and content_type == 'application/json':
+       return document_dict
+
+    return render_template(
+        template_name,
+        **document_dict
+    )
+
--- site/index.py
+++ site/index.py
@@ -6,76 +6,21 @@ from flask import (
     request,
     send_from_directory
 )
+from app.browser import blueprint as browser_blueprint
+
 app = Flask(__name__)
 
 app.jinja_env.auto_reload = True
 
-# Mocks server responses for GET /folder/<int:folder_id>
-FOLDERS = {
-    -1: {
-        'current_nodes': [
-            {'title': 'invoice.pdf', 'id': 5, 'model': 'document'},
-            {'title': 'payment_1.pdf', 'id': 1, 'model': 'document'},
-            {'title': 'payment_2.pdf', 'id': 2, 'model': 'document'},
-            {'title': 'My Documents', 'id': 3, 'model': 'folder'},
-        ],
-        # All ancestors are folders; thus there is no need to specify
-        # model attribute.
-        'ancestor_nodes': [  ]
-    },
-    3: {
-        # notice 'model' attribute which specifies type of node either document
-        # or folder. There can be only two types of nodes: 'document' and
-        # 'folder'
-        'current_nodes': [
-            {'title': 'mydoc1.pdf', 'id': 5, 'model': 'document'},
-            {'title': 'mydoc2.pdf', 'id': 6, 'model': 'document'},
-            {'title': 'Some Folder', 'id': 7, 'model': 'folder'},
-        ],
-        # All ancestors are folders; thus there is no need to specify
-        # model attribute.
-        'ancestor_nodes': [ {'title': 'My Documents', 'id': 3} ]
-    },
-    7: {
-        'current_nodes': [
-            {'title': 'inv1.pdf', 'id': 8, 'model': 'document'},
-            {'title': 'inv2.pdf', 'id': 9, 'model': 'document'},
-            {'title': 'inv3.pdf', 'id': 10, 'model': 'document'},
-            {'title': 'inv4.pdf', 'id': 11, 'model': 'document'},
-        ],
-        # All ancestors are folders; thus there is no need to specify
-        # model attribute.
-        'ancestor_nodes': [
-            {'title': 'My Documents', 'id': 3},
-            {'title': 'Some Folder', 'id': 7},
-        ]
-    }
-}
+app.register_blueprint(
+    browser_blueprint,
+    url_prefix='/mini-browser'
+)
+app.register_blueprint(
+    browser_blueprint,
+    url_prefix='/slow-queries-browsing'
+)
 
-# Mocks server resonses for GET /document/<int:document_id>
-DOCUMENTS = {
-    1: {
-        'document': {
-            'id': 1,
-            'title': 'payment_1.pdf' 
-        },
-        'ancestor_nodes': []
-    },
-    2: {
-        'document': {
-            'id': 2,
-            'title': 'payment_2.pdf' 
-        },
-        'ancestor_nodes': []
-    },
-    5: {
-        'document': {
-            'id': 5,
-            'title': 'invoice.pdf' 
-        },
-        'ancestor_nodes': []
-    },
-}
 
 @app.route('/')
 def index():
@@ -95,94 +40,6 @@ def basic_panel_with_nodes():
 @app.route('/about')
 def about():
     return render_template("about.html")
-
-
-@app.route('/mini-browser')
-def mini_browser():
-    return render_template("features/mini-browser.html")
-
-
-@app.route('/mini-browser/folder/')
-def mini_browser_root_folder():
-    content_type = request.headers.get('Content-Type')
-    if content_type and content_type == 'application/json':
-        return FOLDERS.get(-1)
-
-
-@app.route('/mini-browser/folder/<int:node_id>')
-def mini_browser_folder(node_id):
-    folder_dict = FOLDERS.get(node_id, None)
-    if not folder_dict:
-        return render_template("404.html"), 404
-
-    content_type = request.headers.get('Content-Type')
-    if content_type and content_type == 'application/json':
-       return folder_dict
-
-    return render_template(
-        "features/mini-browser.html", **folder_dict
-    )
-
-
-@app.route('/mini-browser/document/<int:node_id>')
-def mini_browser_document(node_id):
-    document_dict = DOCUMENTS.get(node_id, None)
-    
-    if not document_dict:
-        return render_template("404.html"), 404
-
-    content_type = request.headers.get('Content-Type')
-    if content_type and content_type == 'application/json':
-       return document_dict
-
-    return render_template(
-        "features/mini-browser.html",
-        **document_dict
-    )
-
-
-@app.route('/slow-queries-browsing')
-def slow_quering_browsing():
-    return render_template("features/slow-queries-browsing.html")
-
-
-@app.route('/slow-queries-browsing/folder/')
-def slow_queries_browsing_root_folder():
-    content_type = request.headers.get('Content-Type')
-    if content_type and content_type == 'application/json':
-        return FOLDERS.get(-1)
-
-
-
-@app.route('/slow-queries-browsing/folder/<int:node_id>')
-def slow_queries_browsing_folder(node_id):
-    folder_dict = FOLDERS.get(node_id, None)
-    if not folder_dict:
-        return render_template("404.html"), 404
-
-    content_type = request.headers.get('Content-Type')
-    if content_type and content_type == 'application/json':
-       return folder_dict
-
-    return render_template(
-        "features/slow-queries-browsing.html", **folder_dict
-    )
-
-@app.route('/slow-queries-browsing/document/<int:node_id>')
-def slow_queries_browsing_document(node_id):
-    document_dict = DOCUMENTS.get(node_id, None)
-    
-    if not document_dict:
-        return render_template("404.html"), 404
-
-    content_type = request.headers.get('Content-Type')
-    if content_type and content_type == 'application/json':
-       return document_dict
-
-    return render_template(
-        "features/slow-queries-browsing.html",
-        **document_dict
-    )
 
 
 @app.route('/favicon.ico')
