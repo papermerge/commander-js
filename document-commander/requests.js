22e03acfd7fd4091d89126aff4f7ab62025e1a6c
Eugen Ciur <eugen@papermerge.com>
2021-04-26 08:10:24 +0200

    some comments never hur

--- document-commander/requests.js
+++ document-commander/requests.js
@@ -4,9 +4,41 @@ import { urlconf } from "./urls";
 
 
 function fetch_children(folder) {
-    let options,
-        promise,
-        response;
+    /**
+     Fetches children of given folder via ajax request.
+
+    `folder` parameter can be either `undefined` or an instance of
+     `models.folder.Folder` class.
+    In case `folder` is `undefined` function will retrieve
+    children of the root folder i.e. top most documents and folders
+    for currently logged in user.
+
+    Reponse is expected to be a json in following format:
+        {
+            'current_nodes': [
+                {id, title, model},  # i.e. { id: some_id, title: some_tile, ... }
+                {id, title, model},
+                ...
+            ],
+            'ancestor_nodes': [
+                {id, title}
+            ]
+        }
+
+    'current_nodes' is an array of dictionaries with following keys:
+        - id
+        - title
+        - model
+    Notice that `model` key will be either 'document' or 'folder' depending
+    of the type of node.
+
+    'ancestor_nodes' is an array of dictionaries of nodes ancestors i.e. each
+    node in `ancestor_nodes` is a folder (this is why 'model' key is not present).
+    First node in the list is the most recent ancestor folder provided in argument.
+    Notice that last node in the list of 'ancestor_nodes' is NOT the root folder, as
+    the root folder is assumed to be always there.
+    'ancestor_nodes' are used to render breadcrumbs.
+    */
 
     options = {
         'headers': {
