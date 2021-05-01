6b24408a6113c076e881f1bd79aea0ec98a802aa
Eugen Ciur <eugen@papermerge.com>
2021-05-01 12:36:18 +0200

    CtxMenu.on_node_selected(...)

--- document-commander/models/ctx_menu.js
+++ document-commander/models/ctx_menu.js
@@ -1,14 +1,25 @@
 import { Model } from '../lib/model';
 import { Collection } from "../lib/collection";
 
+import { EV_NODE_SELECTED } from "../events";
+
 class CtxMenu extends Model {
 
     constructor() {
         this.items = new Collection();
+        this.on(EV_NODE_SELECTED, this.on_node_selected, this);
     }
 
     add(action_or_actions) {
         this.items.add(action_or_actions);
+    }
+
+    on_node_selected(node, current_selection) {
+        this.items.forEach((item) => {
+                item.enabled = item.condition(current_selection);
+            }
+        );
+        this.trigger("change");
     }
 }
 
