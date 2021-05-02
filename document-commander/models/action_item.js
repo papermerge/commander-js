a1f7a573a3129e5808a23159ea5546e592eeacaa
Eugen Ciur <eugen@papermerge.com>
2021-05-02 12:00:48 +0200

    some comments never hurt

--- document-commander/models/action_item.js
+++ document-commander/models/action_item.js
@@ -1,4 +1,10 @@
 class ActionItem {
+    /*
+    ActionItem is an item in some sort of menu.
+    It can be an item in context menu (e.g. models.ctx_menu.CtxMenu)
+    or an item in some sort of panel similar functionality like
+    context menu.
+    */
 
     constructor({
         id,
@@ -11,8 +17,16 @@ class ActionItem {
         this.id = id;
         this.title = title;
         this.icon_class = icon_class;
+        /* a callable, which returns true or false
+        `condition` callable will be invoked with
+        {selection, parent} argument.
+        */
         this.condition = condition;
+        // is this item enabled?
         this.enabled = enabled;
+        /* a callable invoked with
+        {selection, parent} arguments
+        */
         this.run = run;
     }
 }
