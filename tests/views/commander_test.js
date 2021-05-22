1f04146d564a8263118674b74ef1bfc8ecfcfa51
Eugen Ciur <eugen@papermerge.com>
2021-05-22 10:26:22 +0200

    fix tests

---
+++ tests/views/commander_test.js
@@ -0,0 +1,29 @@
+import { assert } from "chai";
+
+import { Collection } from "symposium";
+
+import { CommanderView } from "../../document-commander/views/commander";
+
+
+describe("CommanderView test suite", () => {
+
+  it("Can instanciate CommanderView only with options", () => {
+
+    let commander_view = new CommanderView({
+        options:{
+            'panel': {'el': document.querySelector('#panel')},
+            'breadcrumb': {'el': document.querySelector("#breadcrumb")}
+        }
+    });
+
+    assert.isDefined(commander_view);
+  });
+
+  it("Can instanciate CommanderView with empty parameter list", () => {
+
+    let commander_view = new CommanderView();
+
+    assert.isDefined(commander_view);
+  });
+
+});
