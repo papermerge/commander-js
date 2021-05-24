ade21a423fe52c3bdf64e47c929551b8d2678c88
Eugen Ciur <eugen@papermerge.com>
2021-05-24 09:40:06 +0200

    now commander is very well structured!

--- playground/static/js/features/07-open-close.js
+++ playground/static/js/features/07-open-close.js
@@ -38,29 +38,39 @@ window.addEventListener('DOMContentLoaded', () => {
         commander2,
         go,
         which_commander,
-        which_action;
+        which_action,
+        which_folder;
 
     DC.urlconf.prefix = '/07-open-close';
 
     commander1 = new DC.CommanderView({'el': "#commander1"});
     commander2 = new DC.CommanderView({'el': "#commander2"});
+
+    // DOM references
     go = document.querySelector('#go');
     which_action = document.querySelector("#which-action");
     which_commander = document.querySelector("#which-commander");
+    which_folder = document.querySelector("#which-folder");
 
     go.addEventListener('click', () => {
-        let commander;
+        let commander, folder;
 
-        console.log(which_action.value);
-        console.log(which_commander.value);
         if (which_commander.value == "#commander1") {
             commander = commander1;
         } else {
             commander = commander2;
         }
 
+        if (which_folder.value == "-1") {
+            folder = undefined;
+        } else if (which_folder.value == "3") {
+            folder = new DC.Folder({id: 3, title: "My Documents"});
+        } else if (which_folder.value == "7") {
+            folder = new DC.Folder({id: 7, title: "Some Folder"});
+        }
+
         if (which_action.value == "open") {
-            _open(commander, undefined, which_commander.value);
+            _open(commander, folder, which_commander.value);
         } else if ( which_action.value == "close" ) {
             _close(commander);
         } else if (which_action.value == "insert-dummy") {
