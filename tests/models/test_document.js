d77495c0b695609cec19dd5e7da499a3d43a8dc5
Eugen Ciur <eugen@papermerge.com>
2021-07-25 07:02:03 +0200

    change identation level to 4 spaces

--- tests/models/test_document.js
+++ tests/models/test_document.js
@@ -5,26 +5,26 @@ import { Document } from '../../document-commander/models/document';
 
 describe("Document model test suite", () => {
 
-  it("Can instanciate Document", () => {
+    it("Can instanciate Document", () => {
 
-    let doc = new Document({id: 1, title: "invoice.pdf"});
+        let doc = new Document({id: 1, title: "invoice.pdf"});
 
-    assert.isDefined(doc);
-    assert.equal(doc.title, "invoice.pdf");
-    assert.isTrue(doc.is_document);
-    assert.isFalse(doc.is_folder);
-  });
+        assert.isDefined(doc);
+        assert.equal(doc.title, "invoice.pdf");
+        assert.isTrue(doc.is_document);
+        assert.isFalse(doc.is_folder);
+    });
 
-  it("sets by default ocr_status to Document.UNKNOWN", () => {
+    it("sets by default ocr_status to Document.UNKNOWN", () => {
 
-    let doc = new Document({id: 1, title: "invoice.pdf"});
+        let doc = new Document({id: 1, title: "invoice.pdf"});
 
-    assert.isDefined(doc);
-    assert.equal(
-      doc.ocr_status,
-      Document.UNKNOWN,
-      `Initial status is not set to ${Document.UNKNOWN}`
-    );
-  });
+        assert.isDefined(doc);
+        assert.equal(
+            doc.ocr_status,
+            Document.UNKNOWN,
+            `Initial status is not set to ${Document.UNKNOWN}`
+        );
+    });
 
 });
