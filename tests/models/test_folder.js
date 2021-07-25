effbd4544b90247392cee0d3188d37a26eb9dd7d
Eugen Ciur <eugen@papermerge.com>
2021-07-25 06:52:45 +0200

    ident js file by 4 spaces

--- tests/models/test_folder.js
+++ tests/models/test_folder.js
@@ -5,14 +5,13 @@ import { Folder } from '../../document-commander/models/folder';
 
 describe("Folder model test suite", () => {
 
-  it("Can instanciate Folder model", () => {
+    it("Can instanciate Folder model", () => {
+        let folder = new Folder({id: 1, title: "My Documents"});
 
-    let folder = new Folder({id: 1, title: "My Documents"});
-
-    assert.isDefined(folder);
-    assert.equal(folder.title, "My Documents")
-    assert.isTrue(folder.is_folder);
-    assert.isFalse(folder.is_document);
-  });
+        assert.isDefined(folder);
+        assert.equal(folder.title, "My Documents")
+        assert.isTrue(folder.is_folder);
+        assert.isFalse(folder.is_document);
+    });
 
 });
